7be54bcca5db841a7e42bb0f8d76c136f40da66d
Thalles A.S <thalles.al.slv@gmail.com>
2021-05-07 18:18:36 -0300

    Group by search

--- backend/src/main/java/com/phanthasm/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/phanthasm/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.phanthasm.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.phanthasm.dsvendas.dto.SaleDTO;
+import com.phanthasm.dsvendas.dto.SaleSuccessDTO;
+import com.phanthasm.dsvendas.dto.SaleSumDTO;
 import com.phanthasm.dsvendas.entities.Sale;
 import com.phanthasm.dsvendas.repositories.SaleRepository;
 import com.phanthasm.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
